bb6872ae06e3d84eb2339cb51b5dabb1371c58f8
Fadel Chafai <fadelchafai@gmail.com>
2016-05-03 19:24:28 -0100

    Adding comment : Phantomjs doesn't support Object.assign
    install phantomjs-polyfill-object-assign

--- src/app/app.js
+++ src/app/app.js
@@ -1,29 +1,38 @@
 (function(angular) {
-  'use strict';
+    'use strict';
 
-  var __env = {};
+    var __env = {};
 
-  // Import variables if present (from env.js)
-  if (window) {
-    Object.assign(__env, window.__env);
-  }
+    /**
+     * Import variables if present (from env.js)
+     *
+     * Phantomjs doesn't support Object.assign 
+     * Install phantomjs-polyfill-object-assign :
+     * npm install --save-dev phantomjs-polyfill-object-assign
+     * Adding this ligne to karma.conf.js:
+     * // endbower
+     * 'node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js',
+     */
+    if (window) {
+        Object.assign(__env, window.__env);
+    }
 
-  function config($urlRouterProvider, $logProvider, $resourceProvider) {
+    function config($urlRouterProvider, $logProvider, $resourceProvider) {
 
-    $resourceProvider.defaults.stripTrailingSlashes = false;
+        $resourceProvider.defaults.stripTrailingSlashes = false;
 
-    $logProvider.debugEnabled(__env.enableDebug);
+        $logProvider.debugEnabled(__env.enableDebug);
 
-    $urlRouterProvider.otherwise('/');
-  }
+        $urlRouterProvider.otherwise('/');
+    }
 
-  angular.module('myApp', [
-      'ngResource',
-      'myApp.home',
-      'myApp.about'
-    ])
-    .config(config)
-    .constant('__env', __env);
+    angular.module('myApp', [
+            'ngResource',
+            'myApp.home',
+            'myApp.about'
+        ])
+        .config(config)
+        .constant('__env', __env);
 
     config.$inject = ['$urlRouterProvider', '$logProvider', '$resourceProvider'];
 
